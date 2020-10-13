b0ba88aba96908c6231e69456fcf506d4f47d3e8
surya.madhav <surya.madhav@sen-sei.in>
2020-10-14 01:23:57 +0530

    All components added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,24 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { RecipeListComponent } from './recipe/recipe-list/recipe-list.component';
+import { RecipeEditComponent } from './recipe/recipe-edit/recipe-edit.component';
+import { RecipeDetailComponent } from './recipe/recipe-detail/recipe-detail.component';
+import { RecipeItemComponent } from './recipe/recipe-item/recipe-item.component';
+import { RecipesComponent } from './recipe/recipes/recipes.component';
+import { ShopingListComponent } from './shoping/shoping-list/shoping-list.component';
+import { ShopingEditComponent } from './shoping/shoping-edit/shoping-edit.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RecipeListComponent,
+    RecipeEditComponent,
+    RecipeDetailComponent,
+    RecipeItemComponent,
+    RecipesComponent,
+    ShopingListComponent,
+    ShopingEditComponent
   ],
   imports: [
     BrowserModule,
